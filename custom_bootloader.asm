6ce6ba7a21e5a243a52b49be1a52314c11c841d1
ayedaemon <ris3234@gmail.com>
2022-09-28 20:18:50 +0530

    more complex message - failed

--- custom_bootloader.asm
+++ custom_bootloader.asm
@@ -1,15 +1,24 @@
-mov ah, 0x03
+	
+
+; Setup TTY mode
+mov ax, 0x03
 int 10h
 
-mov ah, 0xE
-mov al, 'H'
-int 0x10
 
-mov al, 'E'
-int 0x10
+mov si, msg      ; si register now points to msg
+mov ah, 0Eh      ; Use write function from 10h interrupt
 
-mov al, 'Y'
-int 0x10
+.loop:
+    lodsb          ; load first char from msg and point to next char
+    or al, al      ; Check if end of string
+    jz halt        ; if end of string, jump to halt
+    int 10h        ; else, print char via interrupt
+    jmp .loop      ; loop
+
+halt:
+
+msg: db "Hack the world", 0
 
 times 510-($-$$) db 0
 dw 0xAA55
+
